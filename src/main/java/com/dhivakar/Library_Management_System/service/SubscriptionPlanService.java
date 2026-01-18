d7f503f46e45d77a1569bb847ec1c64d7109ac0a
Dhivakar <dhivakar449@gmail.com>
2026-01-18 16:44:47 +0530

    feat(subscription-plan): add subscription plan and verify endpoints
    Implemented subscription plan functionality and verified all related endpoints using Postman.

---
+++ src/main/java/com/dhivakar/Library_Management_System/service/SubscriptionPlanService.java
@@ -0,0 +1,16 @@
+package com.dhivakar.Library_Management_System.service;
+
+import com.dhivakar.Library_Management_System.payload.dto.SubscriptionPlanDTO;
+
+import java.util.List;
+
+public interface SubscriptionPlanService {
+
+    SubscriptionPlanDTO createSubscriptionPlan(SubscriptionPlanDTO planDTO) throws Exception;
+
+    SubscriptionPlanDTO updateSubscriptionPlan(Long planId, SubscriptionPlanDTO planDTO) throws Exception;
+
+    void deleteSubscriptionPlan(Long planId) throws Exception;
+
+    List<SubscriptionPlanDTO> getAllSubscriptionPlan();
+}
